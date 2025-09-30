9f059bd845769f3a8deb7026ad146c3e4d4b1cb3
Ernest Talagtag <ernestneil09@gmail.com>
2025-10-01 07:54:57 +0800

    PlayerLook: mouse X

--- Assets/_Script/MouseLook.cs
+++ Assets/_Script/MouseLook.cs
@@ -7,10 +7,11 @@ public class MouseLook : MonoBehaviour
 
     private float mouseSensitivity = 100f;
 
+    private float xRotation = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -19,6 +20,12 @@ public class MouseLook : MonoBehaviour
 
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
 
         playerBody.Rotate(Vector3.up * mouseX);
 
