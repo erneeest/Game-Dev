8235a0452a2cfc01698bc53553cabc02750ea658
Ernest Talagtag <ernestneil09@gmail.com>
2025-10-01 00:01:04 +0800

    PlayerLook: mouse X

--- Assets/_Script/MouseLook.cs
+++ Assets/_Script/MouseLook.cs
@@ -3,10 +3,9 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
 
-    // [SerializeField] float mouseSens = 100f;
-
     [SerializeField] Transform playerBody;
-    // [SerializeField] Transform playerCam;
+
+    private float mouseSensitivity = 100f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,13 +16,11 @@ public class MouseLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * 100f * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * 100f * Time.deltaTime;
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         playerBody.Rotate(Vector3.up * mouseX);
-
-        // Debug.Log(mouseX);
-        // Debug.Log(mouseY);
 
     }
 }
