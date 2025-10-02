f3b49ada8f89565b803a37cae920d4f8fe053e69
Ernest Talagtag <ernestneil09@gmail.com>
2025-10-02 20:59:46 +0800

    check sphere

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,17 +1,32 @@
 using System;
+using NUnit.Framework;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
-    float movementSpeed = 20f;
+    float movementSpeed = 3f;
 
-    public float gravity = -9.81f;
+    public float gravity = -9.81f * 2f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
     Vector3 velocity;
+    bool isGrounded;
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f; 
+        }
+
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
 
