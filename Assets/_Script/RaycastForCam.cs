79090cb6d675b3bdefb32092816ace92a8fff3b8
Ernest Talagtag <ernestneil09@gmail.com>
2025-10-05 02:52:44 +0800

    Review Raycast

---
+++ Assets/_Script/RaycastForCam.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class RaycastForCam : MonoBehaviour
+{
+    public float range = 5f;
+    RaycastHit hit;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            // hit.transform.gameObject.SetActive
+        }
+
+        Debug.DrawRay(transform.position, transform.forward * range, Color.red);
+
+    }
+}
