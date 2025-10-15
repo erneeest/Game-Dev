c341f1d1a2a470a59d5d70850446f248a4aec0cb
Ernest Talagtag <ernestneil09@gmail.com>
2025-10-15 18:58:36 +0800

    add interaction with outline asset

--- Assets/_Script/RaycastForCam.cs
+++ Assets/_Script/RaycastForCam.cs
@@ -1,37 +1,6 @@
-using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class RaycastForCam : MonoBehaviour
 {
-    [SerializeField] float range = 5f;
-    public RaycastHit hit;
-    public GameObject hitGameObject;
 
-    [SerializeField] TextMeshProUGUI npcText;
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Physics.Raycast(transform.position, transform.forward, out hit, range))
-        {
-            // Debug.Log(hit.transform.name);
-            hitGameObject = hit.transform.gameObject;
-            CheckInteract();
-        }
-
-        // Debug.DrawRay(transform.position, transform.forward * range, Color.red);
-    }
-
-    void CheckInteract()
-    {
-        if(hit.transform.TryGetComponent<IInteractable>(out var interactable))
-        {
-            interactable.Interact();
-        }
-        else
-        {
-            npcText.text = "";
-        }
-    }
 }
