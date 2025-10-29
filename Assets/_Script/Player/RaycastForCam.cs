ef0dae9545ec1d9ba968812d23f2049e0639839d
Ernest Talagtag <ernestneil09@gmail.com>
2025-10-29 22:46:32 +0800

    Get positions and forward direction of the active cinemachine virtual cam

--- Assets/_Script/Player/RaycastForCam.cs
+++ Assets/_Script/Player/RaycastForCam.cs
@@ -1,3 +1,4 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class RaycastForCam : MonoBehaviour
@@ -9,7 +10,21 @@ public class RaycastForCam : MonoBehaviour
 
     void Update()
     {
-        didHit = Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, raycastRange);
-        Debug.DrawRay(cam.transform.position, cam.transform.forward * raycastRange, Color.green);
-    } 
+        CinemachineBrain brain = gameObject.GetComponent<CinemachineBrain>();
+
+        if (brain != null && brain.ActiveVirtualCamera != null)
+        {
+            ICinemachineCamera activeCam = brain.ActiveVirtualCamera;
+
+            // Get position and forward direction from the active Cinemachine camera state
+            Vector3 camPos = activeCam.State.GetFinalPosition();
+            Vector3 camForward = activeCam.State.GetFinalOrientation() * Vector3.forward;
+
+            // Perform the raycast
+            didHit = Physics.Raycast(camPos, camForward, out hit, raycastRange);
+
+            // Draw debug ray
+            Debug.DrawRay(camPos, camForward * raycastRange, didHit ? Color.red : Color.green);
+        }
+    }
 }
